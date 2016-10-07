191b9025891ddb5c8811400e50c6501b9c7127dd
Brooklyn NY <brooklyn@andulan.com>
2016-10-07 18:30:35 -0400

    Add Example 1 to repo

---
+++ TestConcole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string mydata = "No Name";
+
+        try {
+            Console.Write($"This is myname  {mydata}\n");
+            Console.Write($"My other name is {mydata}\n");
+
+            Console.ReadLine(); /// 
+            
+
+
+            ///This is a pause statement waiting for enter.
+            ///
+            /// 
+            /// 
+            /// 
+            ///
+
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("The process failed: {0}", e.ToString());
+        }
+
+    }
+}
